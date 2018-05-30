d26540d9889e29e04d3f0f9adf6153861ed907da
Ted Lorenzen <lorenze3@gmail.com>
2018-05-29 19:51:55 -0500

    trying to match python to sql outputs

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def signUp():
             cursor = conn.cursor()
             _hashed_password = generate_password_hash(_password)
             #check for records with that username
-            qstr='select * from tbl_user where user_username='+str(_email)+';'
+            qstr='select * from tbl_user where user_username="'+str(_email)+'";'
             cursor.execute(qstr)
             userrecord=cursor.fetchall()
             cursor.close()
