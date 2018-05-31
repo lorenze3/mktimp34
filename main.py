21c009ac72d605029543cfbf3aa00c9155549e64
Ted Lorenzen <lorenze3@gmail.com>
2018-05-30 21:31:31 -0500

    more logging stuff

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def signUp():
             qstr='select * from tbl_user where user_username="'+str(_email)+'";'
             cursor.execute(qstr)
             userrecord=cursor.fetchall()
-            app.logger.info('UserRecord on signUp Check: '+str(userrecord))
+            logging.info('UserRecord on signUp Check: '+str(userrecord))
             cursor.close()
             #return json.dumps({'output':userrecord})
             if len(userrecord) == 0:
@@ -157,7 +157,7 @@ def userHome():
             cursor.callproc('sp_addinputD',(f_name,struid))
             for rr in cursor.stored_results():
                 data=rr.fetchall()
-            app.logger.info('data record on upload check: '+str(data))
+            logging.info('data record on upload check: '+str(data))
             if not('data' in locals()) or data[0][0]!="Existing Filename; Please rename.":
                 #success!
                 triggerModel=1
@@ -254,10 +254,8 @@ def upload():
         return render_template('error.html',error='it worked')#json.dumps({'filename':f_name})
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='smarter.log',level=logging.INFO)
     app.run()
-    handler = RotatingFileHandler('Smarter.log', maxBytes=10000, backupCount=1) 
-    handler.setLevel(logging.INFO) 
-    app.logger.addHandler(handler)  
     
     
     
