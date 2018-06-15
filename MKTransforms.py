0abb8183b9d36f12114abdcd4b9693f031e7fef3
Ted Lorenzen <lorenze3@gmail.com>
2018-06-15 10:01:43 -0500

    more try except blocks to figure out bugs exposed by Darens simple data

--- MKTransforms.py
+++ MKTransforms.py
@@ -10,43 +10,46 @@ def MKTransforms(rawdf):
     #names of id columns, groups for decomps, transforms, knownsigns for models, original dep variable
     #and dataframe with only data that is transformed for modeling 
     """
-    #control information referenced above
-    groups=rawdf.iloc[0,:]
-    transforms=rawdf.iloc[1,:]
-    knownSigns=rawdf.iloc[2,:]
-    
-    #some useful lists for dataframes below
-    needForAdstockVs=[i for i, word in enumerate(transforms) if word.startswith('adstock')]
-    needForAdstockIDs=[i for i,word in enumerate(groups) if word.endswith('id')]
-    needForAdstock=needForAdstockIDs + needForAdstockVs
-    needForLogVs = [i for i, word in enumerate(transforms) if word.startswith('log')]
-    needForMCVs=[i for i, word in enumerate(transforms) if word.endswith('mc')]
-    
-    #get the data
-    
-    
-    #pd.set_option('display.multi_sparse', False)
-    #get the data
-    #rawdf=pd.read_csv("C:/Users/TeamLorenzen/Documents/App0/static/downloads/ex.csv")
-    #make 4 dataframes (or series, I guess)
-    #1) series of groups for decomps from row 2
-    #2) series of transforms from row 3
-    #3) series of sign constraints from row 4
-    
-    #Most Df operations want column names -- so they are made in these lists
-    IDnames=rawdf.columns.values[needForAdstockIDs].tolist()
-    AdstockVs=rawdf.columns.values[needForAdstockVs].tolist()
-    LogVs=rawdf.columns.values[needForLogVs].tolist()
-    MCVs=rawdf.columns.values[needForMCVs].tolist()
-    #if time isn't the last id column then this sort will lead to bad adstock
-    rawdf.sort_values(by=IDnames)
-    
-    #get the data only frame, convert to floats when possible
-    datadf=rawdf.iloc[3:rawdf.shape[0],:]
-    #get original dependent in a dataframe to be returned for use in decomps
-    idxdep=[i for i,word in enumerate(groups) if word==('dependent')]
-    depV=rawdf.columns.values[idxdep].tolist()
-    origDep=datadf[depV]
+    try:
+        #control information referenced above
+        groups=rawdf.iloc[0,:]
+        transforms=rawdf.iloc[1,:]
+        knownSigns=rawdf.iloc[2,:]
+        
+        #some useful lists for dataframes below
+        needForAdstockVs=[i for i, word in enumerate(transforms) if word.startswith('adstock')]
+        needForAdstockIDs=[i for i,word in enumerate(groups) if word.endswith('id')]
+        needForAdstock=needForAdstockIDs + needForAdstockVs
+        needForLogVs = [i for i, word in enumerate(transforms) if word.startswith('log')]
+        needForMCVs=[i for i, word in enumerate(transforms) if word.endswith('mc')]
+        
+        #get the data
+        
+        
+        #pd.set_option('display.multi_sparse', False)
+        #get the data
+        #rawdf=pd.read_csv("C:/Users/TeamLorenzen/Documents/App0/static/downloads/ex.csv")
+        #make 4 dataframes (or series, I guess)
+        #1) series of groups for decomps from row 2
+        #2) series of transforms from row 3
+        #3) series of sign constraints from row 4
+        
+        #Most Df operations want column names -- so they are made in these lists
+        IDnames=rawdf.columns.values[needForAdstockIDs].tolist()
+        AdstockVs=rawdf.columns.values[needForAdstockVs].tolist()
+        LogVs=rawdf.columns.values[needForLogVs].tolist()
+        MCVs=rawdf.columns.values[needForMCVs].tolist()
+        #if time isn't the last id column then this sort will lead to bad adstock
+        rawdf.sort_values(by=IDnames)
+        
+        #get the data only frame, convert to floats when possible
+        datadf=rawdf.iloc[3:rawdf.shape[0],:]
+        #get original dependent in a dataframe to be returned for use in decomps
+        idxdep=[i for i,word in enumerate(groups) if word==('dependent')]
+        depV=rawdf.columns.values[idxdep].tolist()
+        origDep=datadf[depV]
+    except Exception as e:
+        print("in dataset up:" + e)
     #Adstock transform -- everyone gets a .5 for now!
     retention=0.5
     #forAdstock=rawdf.iloc[3:rawdf.shape[0],needForAdstock]
