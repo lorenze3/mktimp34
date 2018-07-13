f6b3bc4e37817ead752bb3229aee3e23c9f1e6b1
Ted Lorenzen <lorenze3@gmail.com>
2018-07-13 13:49:18 -0500

    repaired Decomp0 for log models by making sure original dep was inserted at front of decomp dataset instead of at end

--- MKTransforms.py
+++ MKTransforms.py
@@ -248,6 +248,7 @@ def runModels(depV,IDnames,groups, knownSigns, origDep,datadf):
     intcoef=r_[int,coef]
     return intcoef, X1, Y1
 
+
 def decomp0(X1,Y1,origDep,intcoef,depV,depMeans,transforms,rawdf,IDnames):
     import math
     import pandas as pd
@@ -296,7 +297,7 @@ def decomp0(X1,Y1,origDep,intcoef,depV,depMeans,transforms,rawdf,IDnames):
             origSpaceDecomp=pd.DataFrame(0,index=plainidx,columns=modSpaceDecomp.columns.values)
             for C in modSpaceDecomp:
                 origSpaceDecomp[C]=modSpaceDecomp[C].apply(lambda x:math.exp(x))
-        elif transformsDict[depV[0]].endswith('mc'):
+        elif transformsDict[depV[0]]=='mc':
             origSpaceDecomp=pd.DataFrame(0,index=plainidx,columns=modSpaceDecomp.columns.values)
             origSpaceDecomp=modSpaceDecomp #starting point is modspace, now add in mean values for depV
             #make depMeans a DF
@@ -315,9 +316,28 @@ def decomp0(X1,Y1,origDep,intcoef,depV,depMeans,transforms,rawdf,IDnames):
             #that's a lot of fucking work to add in a column, finally apply the exp
             #first drop depV[0] column, no one knows what it is at this point
             origSpaceDecomp.drop(depV[0],axis=1,inplace=True)
-            if transformsDict[depV[0]]=='logmc':
-                for C in origSpaceDecomp:
-                    origSpaceDecomp[C]=origSpaceDecomp[C].apply(lambda x:math.exp(x))
+        elif transformsDict[depV[0]]=='logmc':
+            origSpaceDecomp=pd.DataFrame(0,index=plainidx,columns=modSpaceDecomp.columns.values)
+            origSpaceDecomp=modSpaceDecomp #starting point is modspace, now add in mean values for depV
+            #make depMeans a DF
+            depMeans=pd.DataFrame(depMeans)
+            #reset index on decomp data frame for differencing
+            #first insert the columns back from rawdf
+            idCols=rawdf.loc[3:rawdf.shape[0],IDnames[0:len(IDnames)-1]]
+            origSpaceDecomp=origSpaceDecomp.join(idCols,how='outer')
+            origSpaceDecomp.set_index(keys=IDnames[0:len(IDnames)-1],inplace=True)
+            #make a dataframe with onecolumn
+            intOnesPlus=pd.DataFrame(origSpaceDecomp['intOnes']+depMeans[depV[0]],columns=['intOnes']).set_index(plainidx)
+            #drop the old column, insert the new column
+            origSpaceDecomp.drop('intOnes',axis=1,inplace=True)
+            origSpaceDecomp.insert(loc=0,column='intOnes',value=intOnesPlus.values)
+            origSpaceDecomp.set_index(keys=plainidx,inplace=True)
+            #that's a lot of fucking work to add in a column, finally apply the exp
+            #first drop depV[0] column, no one knows what it is at this point
+            origSpaceDecomp.drop(depV[0],axis=1,inplace=True)
+            for C in origSpaceDecomp:
+                origSpaceDecomp[C]=origSpaceDecomp[C].apply(lambda x:math.exp(x))
+            
     except Exception as e:
         print('Reverse Transform Failure:' + e)
     #ok, we are done if additive model
@@ -359,13 +379,13 @@ def decomp0(X1,Y1,origDep,intcoef,depV,depMeans,transforms,rawdf,IDnames):
             origY1minus.set_index(keys=IDnames[0:len(IDnames)-1],inplace=True)
             #make a dataframe with onecolumn
             origY1=pd.DataFrame(origY1minus[depV[0]]+depMeans[depV[0]],columns=[depV[0]]).set_index(plainidx)
+            origY1[depV[0]]=origY1[depV[0]].apply(lambda x:math.exp(pd.to_numeric(x)))
         elif transformsDict[depV[0]]=='none':
             origY1=Y1
         #now append the origY1[depV[0]]
         
-        #origSpaceDecomp.drop(depV[0],axis=1)
-        #origSpaceDecomp.insert(loc=0,column=depV[0],value=origY1[depV[0]].values)
-        origSpaceDecomp[depV[0]]=origY1
+        origSpaceDecomp.insert(loc=0,column=depV[0],value=origY1[depV[0]].values)
+        print('bad'+str(origSpaceDecomp.shape[1]))
         origSpaceDecomp['total']=origSpaceDecomp.iloc[:,1:origSpaceDecomp.shape[1]].sum(axis=1)
         origSpaceDecomp['residual']=origSpaceDecomp[depV[0]]-origSpaceDecomp['total']
         origSpaceDecomp.drop('total',axis=1,inplace=True)
